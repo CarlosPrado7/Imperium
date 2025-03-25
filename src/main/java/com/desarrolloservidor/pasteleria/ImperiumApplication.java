ae8baf67c838353670cf0bc2dc67e59b46080f84
Carlos <carlosprado101103@gmail.com>
2025-03-25 11:18:30 +0100

    Estructura

---
+++ src/main/java/com/desarrolloservidor/pasteleria/ImperiumApplication.java
@@ -0,0 +1,13 @@
+package com.desarrolloservidor.pasteleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiumApplication.class, args);
+	}
+
+}
